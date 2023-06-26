d2ab18cf5fa246cae7337217b2eb498bf08043c0
Aditya Roy Chowdhury <adityaroychowdhury@Adityas-MacBook-Air.local>
2023-06-26 10:42:20 -0400

    ch

--- vueintro/main.js
+++ vueintro/main.js
@@ -84,20 +84,46 @@ Vue.component('product', {
 
 Vue.component('product-review', {
     template: `
-        <p>
-            <label for="name">Name</label>
-            <input id ="name" v-model="name">
-        </p>
-        <p>
-            <label for="review">Review</label>
-            <textarea id ="name" v-model="name"></textarea>
-        </p> 
-    `,
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+            </p>
+            
+            <p>
+            <label for="review">Review:</label>      
+            <textarea id="review" v-model="review"></textarea>
+            </p>
+            
+            <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+            </p>
+                
+            <p>
+            <input type="submit" value="Submit">  
+            </p>      
+        </form>`,
     data() {
         return {
             name: null,
             review: null,
             rating: null
+        }
+    },
+    methods: {
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
         }
     }
 })
